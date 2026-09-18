1e7b70caf76716e367a7340b8c1c989bfb6b97ed
Aluno <e023aluno@outlook.com>
2026-09-18 19:32:33 -0300

    DB.JS-PRONTO

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -5,5 +5,18 @@ export const pool = mysql.createPool({
     host: process.env.DB_HOST,
     port: Number(process.env.DB_POT || 3306),
     user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD
-})
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+
+    // define se quem chegar depois espera;
+    wautForConnections: true,
+    // define quantas conexões trabalhamao mesmo tempo;
+    conecctionLimit: 10,
+    // queueLimit define quantos podem ficar nessa espera.
+    queueLimit: 0
+
+    // é basicamente "Trabalhe com até 10 conexões;
+    // // se lotar, espere; e nãoimponha um limite de tamanho à fila."
+
+    // O queueLimit: 0 não siginifica "sem fila"; significa fila sem limite  configurado.
+});
