89ada349414077e222619e94a1e6606d017a3dfd
https://github.com/tiagosatilio25-max/git.h.git <tiagosatilio25@gmail.com>
2026-09-20 01:20:09 -0300

    feat: server.js pronto

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,4 +5,30 @@ import authRoutes from "./routes/authRoutes.js";
 import { pool } from "./config/db.js";
 
 const app = express();
-const PORT = process.env.POT || 3000;
+const PORT = process.env.PORT || 3000;
+
+app.use(cors({ origin: "http://localhost:5173" }));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ mensagem: "API de autenticação funcionando" });
+});
+
+app.use(authRoutes);
+
+async function iniciar() {
+  try {
+    const connection = await pool.getConnection();
+    console.log("MySQL conectado com sucesso");
+    connection.release();
+
+    app.listen(PORT, () => {
+      console.log(`Servidor rodando em http://localhost:${PORT}`);
+    });
+  } catch (erro) {
+    console.error("Erro ao conectar no MySQL:", erro.message);
+    process.exit(1);
+  }
+}
+
+iniciar();
